7d85ffa2464cef602c253d138694d61752ea768d
Piotr Owsiak <piotr.owsiak@partners.mbank.pl>
2014-06-23 12:59:45 +0200

    form title and tray icon title changed

--- KeyboardHooks/FormMain.Designer.cs
+++ KeyboardHooks/FormMain.Designer.cs
@@ -78,7 +78,7 @@
             // 
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "Keyboard Hooks";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
@@ -131,7 +131,7 @@
             this.Controls.Add(this.lbText);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormMain";
-            this.Text = "FormMain";
+            this.Text = "Keybord Hooks";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
